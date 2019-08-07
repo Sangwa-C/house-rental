25d192b6ee08d725475860de038a0e8b5c1935eb
angelicty <muyenziraissa@gmail.com>
2019-08-07 12:56:56 +0200

    styling the second gallery

--- js/scripts.js
+++ js/scripts.js
@@ -30,36 +30,35 @@ $(document).ready(function(){
     });
     $("#house2").click(function(){
         $("#house1").hide();
-       })
-    
+       });
+       $(".back").click(function(){
+           $(".gallery").hide();
+           $(".menu").show();
+       });
     });
-    var slideIndex = 1;
-    showSlides(slideIndex);
-    
-    // Next/previous controls
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-    
-    // Thumbnail image controls
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("demo");
-      var captionText = document.getElementById("caption");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-      captionText.innerHTML = dots[slideIndex-1].alt;
-    }
+
+
+  function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+function myFunctionOne(imgs) {
+    // Get the expanded image
+    var expandImg1 = document.getElementById("expandedImg1");
+    // Get the image text
+    var imgText1 = document.getElementById("imgtext1");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg1.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText1.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg1.parentElement.style.display = "block"; 
+}
