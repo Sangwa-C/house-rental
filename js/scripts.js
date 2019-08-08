66d1d6a86046ab7d23ec2ba1ecb690a6ed3fba0b
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-08-08 11:29:10 +0200

    added the alert jquery on the form

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,19 @@ function menuClose() {
 }
 
 
+$(document).ready(function(){
+  $("#send").click(function(){
+    var name=$("input#name").val();
+    var email=$("input#email").val();
+    var subject=$("input#subject").val();
+    var message=$("#message").val();
+    if ((name!=="") &&  (subject!=="") && (email!=="") && (message!=="")){
+      alert( "Hello " + name + "! We have received your message. Thank you for reaching out to us.")
+    }
+    else{
+      alert("Fill well the form")
+    }
+       
+  });
+});
+  
